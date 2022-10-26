35c2295b896baaf992cbcba00f5b8627e750e2ca
Edwin Xu <edwinhuish@gmail.com>
2022-10-26 18:39:32 +0800

    更新配置文件

--- config/rbac.php
+++ config/rbac.php
@@ -39,7 +39,7 @@ return [
     | the role if it is in a different namespace.
     |
     */
-    'role' => \app\admin\model\Role::class,
+    'role' => \Edwinhuish\ThinkRbac\Models\Role::class,
 
     /*
     |--------------------------------------------------------------------------
@@ -60,7 +60,7 @@ return [
     | Update the permission if it is in a different namespace.
     |
     */
-    'permission' => \app\admin\model\Permission::class,
+    'permission' => \Edwinhuish\ThinkRbac\Models\Permission::class,
 
     /*
     |--------------------------------------------------------------------------
